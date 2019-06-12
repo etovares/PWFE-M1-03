d363150eef978fcf7f4171ac95e21043f2c86593
--global <--global>
2019-06-11 21:37:49 -0300

    Pasando de JSON a objetos

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-let http = require("http")
-let fs= require("fs")
+const http = require("http");
+const fs = require("fs");
 
-http.createServer(function(peticion,respuesta){
+http.createServer((request, response) => {
+		
+		let dir = "public/";
 
-let archivo= peticion.url //<--/index.html
+		let file = (request.url == "/") ? "index.html" : request.url;
+			file = (file.match(/[^.]+(\.[^?#]+)?/) || [])[0];
+		
+		let ext = file.substring( file.lastIndexOf(".") ).toLowerCase();
 
-//archivo= archivo.substr(1)
+		let types = {
+			".html"	: "text/html",
+			".js"	: "text/javascript",
+			".css"	: "text/css",
+			".txt" 	: "text/plain",
+			".json"	: "application/json",
+			".png"	: "image/png",
+			".jpg"	: "image/jpg",
+			".gif"	: "image/gif",
+			".ico"	: "image/x-icon",
+			".wav"	: "audio/wav",
+			".mp4"	: "video/mp4",
+			".woff"	: "application/font-woff",
+			".ttf"	: "application/font-ttf",
+			".eot"	: "application/vnd.ms-fontobject",
+			".otf"	: "application/font-otf",
+			".svg"	: "application/image/svg+xml"
+		};
 
-fs.readFile("public" + archivo,function(error,file){
-if (error){
-      respuesta.end("404 - Archivo no encontrado:(")
-}
-      	else{
-      respuesta.end(file)
-      	
+		let contentType = types[ext] || "application/octet-stream";
 
-}
-})
-}).listen(2000)
+		fs.readFile( dir + file, (error, content) => {
+			
+			if ( error ) {
+				response.writeHead(404, { "Content-Type" : "text/plain" } );
+				response.end("ARCHIVO NO ENCONTRADO");
+			} else {
+				response.writeHead(200, { "Content-Type" : contentType } );
+				response.end(content);
+			}
+
+		});
+
+}).listen(2000);
